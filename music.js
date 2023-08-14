41a6a5c6d11f852bbf7626be06f75004525deed5
darshakvaska <go2darshak17@gmail.com>
2023-08-14 21:37:11 +0530

    Added express to application

--- music.js
+++ music.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const express = require('express')
+const app = express()
+const port = 3006
+
+app.get('/', (req, res) => {
+  res.send('Hello World! hello')
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World! hello')
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World! hello')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
